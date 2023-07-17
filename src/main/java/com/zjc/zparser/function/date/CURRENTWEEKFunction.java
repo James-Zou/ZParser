9e87fa836c2855730fc066954e804562ad00f312
RodeRick <18301545237@163.com>
2023-07-17 21:57:20 +0800

    Add 完善license

---
+++ src/main/java/com/zjc/zparser/function/date/CURRENTWEEKFunction.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 James Zou.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zjc.zparser.function.date;
+
+import com.zjc.zparser.actuator.ExecutionCenter;
+import com.zjc.zparser.enums.DateFunctionTypeEnum;
+import com.zjc.zparser.function.BaseFunction;
+import com.zjc.zparser.function.FunctionConstants;
+import com.zjc.zparser.function.FunctionException;
+import com.zjc.zparser.function.FunctionResult;
+import com.zjc.zparser.utils.DateUtil;
+
+/**
+ * @author James.Zou
+ * @Description:获取当前时间是周几
+ *
+ * @date 6/16/23 3:42 PM
+ */
+public class CURRENTWEEKFunction extends BaseFunction {
+    @Override
+    public String getName() {
+        return DateFunctionTypeEnum.CURRENTWEEK_FUNCTION.getType();
+    }
+
+    @Override
+    public FunctionResult execute(ExecutionCenter executionCenter, String arguments) throws FunctionException {
+        String result = null;
+        String exceptionMessage = "不需要参数 ";
+
+
+
+        try {
+            result = DateUtil.getWeek();
+        } catch (Exception e) {
+            throw new FunctionException(exceptionMessage, e);
+        }
+        return new FunctionResult(result,
+                FunctionConstants.FUNCTION_RESULT_TYPE_DATE);
+    }
+}
--- src/main/java/com/zjc/zparser/parser/ModulusOperator.java
+++ src/main/java/com/zjc/zparser/parser/ModulusOperator.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2023 James Zou.
+ * Copyright 2002-2007 Robert Breidecker.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
